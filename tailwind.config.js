87e7955c0707b1f975628a672ca765f8620f7a1a
mryaxyobek <pubgn9642@gmail.com>
2023-10-10 21:30:01 +0500

    added home page primary colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,19 @@ export default {
   theme: {
     extend: {
       colors: {
-        'dark': '#111',
+        primary: {
+          'red-60': '#AE0712',
+          'red-20': '#DA0916',
+          'red-opacity-20': '#F20C0C',
+          'gray-90': '#222',
+          'gray-80': '#333',
+          'gray-70': '#4D4D4D',
+          'gray-20': '#CCCCCC',
+          'gray-04': '#F5F5F5',
+          'success-70': '#039855',
+          'success-50': '#32D583',
+          'warning-50': '#FFA800',
+        },
       },
     },
   },
