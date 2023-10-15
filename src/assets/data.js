25dc1e08b03cc2cfe427f7ce53d064557137cf25
mryaxyobek <pubgn9642@gmail.com>
2023-10-15 15:27:01 +0500

    Added new data to data.js

---
+++ src/assets/data.js
@@ -0,0 +1,307 @@
+// buttons 
+export const tabButtonsGroup = [
+    {
+        id: 1,
+        text: 'Все',
+    },
+    {
+        id: 2,
+        text: 'Кабинеты для руководителя',
+    },
+    {
+        id: 3,
+        text: 'Мебель для персонала',
+    },
+    {
+        id: 4,
+        text: 'Офисные кресла',
+    },
+    {
+        id: 5,
+        text: 'Офисные диваны',
+    },
+    {
+        id: 6,
+        text: 'Стойки рецепшн',
+    },
+    {
+        id: 7,
+        text: 'Столы для переговоров',
+    },
+    {
+        id: 8,
+        text: 'Еще',
+    }
+]
+
+// products
+export const products = [
+    {
+        id: 1,
+        type: 'Мебель для персонала',
+        productId: '209723',
+        country: 'Турция',
+        title: 'Мебель для персонала Metall System Style',
+        tableMaterial: 'ЛДСП',
+        tableThicknessn: '18',
+        currentPrice: '11 990₽',
+        oldPrice: '25 990₽',
+        order: false,
+        new: true,
+        aksiya: false,
+        available: true,
+        images: [
+            {
+                id: 1,
+                img: 'https://assets.architecturaldigest.in/photos/6008351194ca758bb0ad0d69/16:9/w_2560%2Cc_limit/office-furniture-main-1366x768.jpg',
+            },
+            {
+                id: 2,
+                img: 'https://www.shutterstock.com/image-photo/office-furniture-on-white-background-260nw-390277798.jpg',
+            },
+            {
+                id: 3,
+                img: 'https://img.freepik.com/premium-photo/light-office-furniture-middle-room-office-room-layout-3d-illustration_724548-977.jpg',
+            },
+
+            {
+                id: 4,
+                img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmhV2y9Ejt8c7oajXsGr01kmhiSuEYNJkzd3Lc6EXW6D_ZPmgI4gQayMgbLrdjnhn9NDY&usqp=CAU',
+            }
+        ]
+    },
+    {
+        id: 2,
+        type: 'Мебель для персонала',
+        productId: '209723',
+        title: 'Мебель для персонала Metall System Style',
+        country: 'Турция',
+        tableMaterial: 'ЛДСП',
+        tableThicknessn: '18',
+        currentPrice: '11 990₽',
+        oldPrice: false,
+        order: false,
+        new: true,
+        aksiya: false,
+        available: true,
+        images: [
+            {
+                id: 1,
+                img: 'https://www.shutterstock.com/image-photo/office-furniture-on-white-background-260nw-390277798.jpg',
+            },
+            {
+                id: 2,
+                img: 'https://assets.architecturaldigest.in/photos/6008351194ca758bb0ad0d69/16:9/w_2560%2Cc_limit/office-furniture-main-1366x768.jpg',
+            },
+            {
+                id: 3,
+                img: 'https://img.freepik.com/premium-photo/light-office-furniture-middle-room-office-room-layout-3d-illustration_724548-977.jpg',
+            },
+
+            {
+                id: 4,
+                img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmhV2y9Ejt8c7oajXsGr01kmhiSuEYNJkzd3Lc6EXW6D_ZPmgI4gQayMgbLrdjnhn9NDY&usqp=CAU',
+            }
+        ]
+    },
+    {
+        id: 3,
+        type: 'Мебель для персонала',
+        productId: '209723',
+        title: 'Мебель для персонала Metall System Style',
+        country: 'Турция',
+        tableMaterial: 'ЛДСП',
+        tableThicknessn: '18',
+        currentPrice: '11 990₽',
+        oldPrice: '25 990₽',
+        order: false,
+        new: true,
+        aksiya: false,
+        available: true,
+        images: [
+            {
+                id: 1,
+                img: 'https://assets.architecturaldigest.in/photos/6008351194ca758bb0ad0d69/16:9/w_2560%2Cc_limit/office-furniture-main-1366x768.jpg',
+            },
+            {
+                id: 2,
+                img: 'https://img.freepik.com/premium-photo/light-office-furniture-middle-room-office-room-layout-3d-illustration_724548-977.jpg',
+            },
+            {
+                id: 3,
+                img: 'https://www.shutterstock.com/image-photo/office-furniture-on-white-background-260nw-390277798.jpg',
+            },
+            {
+                id: 4,
+                img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmhV2y9Ejt8c7oajXsGr01kmhiSuEYNJkzd3Lc6EXW6D_ZPmgI4gQayMgbLrdjnhn9NDY&usqp=CAU',
+            }
+        ]
+    },
+    {
+        id: 4,
+        type: 'Мебель для персонала',
+        productId: '209723',
+        title: 'Мебель для персонала Metall System Style',
+        country: 'Турция',
+        tableMaterial: 'ЛДСП',
+        tableThicknessn: '18',
+        currentPrice: '11 990₽',
+        oldPrice: false,
+        order: false,
+        new: false,
+        aksiya: true,
+        available: true,
+        images: [
+            {
+                id: 1,
+                img: 'https://assets.architecturaldigest.in/photos/6008351194ca758bb0ad0d69/16:9/w_2560%2Cc_limit/office-furniture-main-1366x768.jpg',
+            },
+            {
+                id: 2,
+                img: 'https://www.shutterstock.com/image-photo/office-furniture-on-white-background-260nw-390277798.jpg',
+            },
+            {
+                id: 3,
+                img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmhV2y9Ejt8c7oajXsGr01kmhiSuEYNJkzd3Lc6EXW6D_ZPmgI4gQayMgbLrdjnhn9NDY&usqp=CAU',
+            },
+            {
+                id: 4,
+                img: 'https://img.freepik.com/premium-photo/light-office-furniture-middle-room-office-room-layout-3d-illustration_724548-977.jpg',
+            },
+        ]
+    },
+    {
+        id: 5,
+        type: 'Мебель для персонала',
+        productId: '209723',
+        title: 'Мебель для персонала Metall System Style',
+        country: 'Турция',
+        tableMaterial: 'ЛДСП',
+        tableThicknessn: '18',
+        currentPrice: '11 990₽',
+        oldPrice: '25 990₽',
+        order: false,
+        new: true,
+        aksiya: false,
+        available: true,
+        images: [
+            {
+                id: 1,
+                img: 'https://assets.architecturaldigest.in/photos/6008351194ca758bb0ad0d69/16:9/w_2560%2Cc_limit/office-furniture-main-1366x768.jpg',
+            },
+            {
+                id: 2,
+                img: 'https://img.freepik.com/premium-photo/light-office-furniture-middle-room-office-room-layout-3d-illustration_724548-977.jpg',
+            },
+            {
+                id: 3,
+                img: 'https://www.shutterstock.com/image-photo/office-furniture-on-white-background-260nw-390277798.jpg',
+            },
+            {
+                id: 4,
+                img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmhV2y9Ejt8c7oajXsGr01kmhiSuEYNJkzd3Lc6EXW6D_ZPmgI4gQayMgbLrdjnhn9NDY&usqp=CAU',
+            }
+        ]
+    },
+    {
+        id: 6,
+        type: 'Мебель для персонала',
+        productId: '209723',
+        country: 'Турция',
+        title: 'Мебель для персонала Metall System Style',
+        tableMaterial: 'ЛДСП',
+        tableThicknessn: '18',
+        currentPrice: '11 990₽',
+        oldPrice: '25 990₽',
+        order: false,
+        new: true,
+        aksiya: false,
+        available: true,
+        images: [
+            {
+                id: 1,
+                img: 'https://assets.architecturaldigest.in/photos/6008351194ca758bb0ad0d69/16:9/w_2560%2Cc_limit/office-furniture-main-1366x768.jpg',
+            },
+            {
+                id: 2,
+                img: 'https://www.shutterstock.com/image-photo/office-furniture-on-white-background-260nw-390277798.jpg',
+            },
+            {
+                id: 3,
+                img: 'https://img.freepik.com/premium-photo/light-office-furniture-middle-room-office-room-layout-3d-illustration_724548-977.jpg',
+            },
+
+            {
+                id: 4,
+                img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmhV2y9Ejt8c7oajXsGr01kmhiSuEYNJkzd3Lc6EXW6D_ZPmgI4gQayMgbLrdjnhn9NDY&usqp=CAU',
+            }
+        ]
+    },
+    {
+        id: 7,
+        type: 'Мебель для персонала',
+        productId: '209723',
+        title: 'Мебель для персонала Metall System Style',
+        country: 'Турция',
+        tableMaterial: 'ЛДСП',
+        tableThicknessn: '18',
+        currentPrice: '11 990₽',
+        oldPrice: false,
+        order: false,
+        new: true,
+        aksiya: false,
+        available: true,
+        images: [
+            {
+                id: 1,
+                img: 'https://www.shutterstock.com/image-photo/office-furniture-on-white-background-260nw-390277798.jpg',
+            },
+            {
+                id: 2,
+                img: 'https://assets.architecturaldigest.in/photos/6008351194ca758bb0ad0d69/16:9/w_2560%2Cc_limit/office-furniture-main-1366x768.jpg',
+            },
+            {
+                id: 3,
+                img: 'https://img.freepik.com/premium-photo/light-office-furniture-middle-room-office-room-layout-3d-illustration_724548-977.jpg',
+            },
+
+            {
+                id: 4,
+                img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmhV2y9Ejt8c7oajXsGr01kmhiSuEYNJkzd3Lc6EXW6D_ZPmgI4gQayMgbLrdjnhn9NDY&usqp=CAU',
+            }
+        ]
+    },
+    {
+        id: 8,
+        type: 'Мебель для персонала',
+        productId: '209723',
+        title: 'Мебель для персонала Metall System Style',
+        country: 'Турция',
+        tableMaterial: 'ЛДСП',
+        tableThicknessn: '18',
+        currentPrice: '11 990₽',
+        oldPrice: false,
+        order: false,
+        new: false,
+        aksiya: true,
+        available: true,
+        images: [
+            {
+                id: 1,
+                img: 'https://assets.architecturaldigest.in/photos/6008351194ca758bb0ad0d69/16:9/w_2560%2Cc_limit/office-furniture-main-1366x768.jpg',
+            },
+            {
+                id: 2,
+                img: 'https://www.shutterstock.com/image-photo/office-furniture-on-white-background-260nw-390277798.jpg',
+            },
+            {
+                id: 3,
+                img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmhV2y9Ejt8c7oajXsGr01kmhiSuEYNJkzd3Lc6EXW6D_ZPmgI4gQayMgbLrdjnhn9NDY&usqp=CAU',
+            },
+            {
+                id: 4,
+                img: 'https://img.freepik.com/premium-photo/light-office-furniture-middle-room-office-room-layout-3d-illustration_724548-977.jpg',
+            },
+        ]
+    },
+]
