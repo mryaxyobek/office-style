886857ade5d5692f01d6ed2f0404a9fbfed43117
mryaxyobek <pubgn9642@gmail.com>
2023-10-20 23:23:20 +0500

    added contact section for about payment page

---
+++ src/components/PaymentPageContactSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Contact from './Contact'
+
+const PaymentPageContactSection = () => {
+    return (
+        <section className='pt-60px pb-120px'>
+            <div className="container">
+                <h2 className="mb-60px">Контакты</h2>
+                <Contact />
+            </div>
+        </section>
+    )
+}
+
+export default PaymentPageContactSection
--- src/pages/AboutPayment.jsx
+++ src/pages/AboutPayment.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import DeliveryAndPayment from '../components/DeliveryAndPayment';
+import PaymentPageContactSection from '../components/PaymentPageContactSection';
 
 const AboutPayment = () => {
     return (
         <>
             <DeliveryAndPayment />
+            <PaymentPageContactSection />
         </>
     )
 };
