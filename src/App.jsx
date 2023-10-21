5906daa5935453d5f765bcff3ac806d053c5f72d
mryaxyobek <pubgn9642@gmail.com>
2023-10-21 09:34:40 +0500

    added "about company" page

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,21 @@ import React from 'react';
 import { RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 
-// pages 
-import Home from './pages/Home';
-
 // layouts 
 import MainRoot from './layouts/MainRoot';
+
+// pages 
+import Home from './pages/Home';
 import AboutPayment from './pages/AboutPayment';
 import ContactPage from './pages/ContactPage';
+import AboutCompanyPage from './pages/AboutCompanyPage';
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<MainRoot />}>
         <Route index element={<Home />} />
+        <Route path='/about-company' element={<AboutCompanyPage />} />
         <Route path='/about-payment' element={<AboutPayment />} />
         <Route path='/contact' element={<ContactPage />} />
       </Route>
---
+++ src/pages/AboutCompanyPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AboutCompanyPage = () => {
+    return (
+        <>
+        </>
+    )
+};
+
+export default AboutCompanyPage;
