28d8f6d27fd5d7ca1fb89c1475d4133fa437ca70
mryaxyobek <pubgn9642@gmail.com>
2023-11-01 16:44:10 +0500

    "completed projects" section(component) is optimized for all devices

--- src/components/CompletedProjects.jsx
+++ src/components/CompletedProjects.jsx
@@ -18,33 +18,35 @@ import { EffectFade, Navigation, Pagination } from 'swiper/modules';
 
 const CompletedProjects = () => {
     return (
-        <div className="pt-36 pb-120px">
+        <div className="pt-36r pb-120px max-900:pb-24 max-440:pb-16">
             <div className="container">
-                <h1 className="mb-60px">Выполненные проекты</h1>
-                <ul className="grid grid-cols-1 gap-60px completed-products-list">
+                <h1 className="mb-60r">Выполненные проекты</h1>
+                <ul className="grid grid-cols-1 gap-60px completed-products-list max-1200:gap-14 max-1050:gap-12 max-900:gap-10 max-440:gap-8">
                     {
                         completedProjectsData.map((e) => {
                             return (
-                                <li key={e.id} className='grid grid-cols-2 gap-8 p-6 bg-primary-gray-04 rounded-2.5xl'>
+                                <li key={e.id} className='grid grid-cols-2 gap-8 p-6 bg-primary-gray-04 rounded-2.5xl max-640:p-0'>
 
                                     {/* first child  */}
-                                    <div className='flex flex-col items-start'>
+                                    <div className='flex flex-col items-start max-640:p-6 max-640:!pt-0 max-540:p-5 max-440:p-4'>
                                         {/* title  */}
-                                        <h3 className="text-regular-36 mb-9">{e.title}</h3>
+                                        <h3 className="text-regular-36 mb-9 max-900:text-regular-32 max-900:mb-8 max-640:mb-7 max-640:text-regular-28 max-440:text-regular-24 max-440:mb-6 max-360:text-regular-20">{e.title}</h3>
 
                                         {/* table  */}
-                                        <table className="w-full mb-9">
+                                        <table className="w-full mb-9 max-900:mb-8 max-640:mb-7 max-440:mb-6">
                                             <tbody>
                                                 {
                                                     e.details.map((detail) => {
                                                         return (
                                                             <tr key={detail.id} className="inline-block w-full first:border-t py-2 border-b border-primary-gray-20">
-                                                                <td className="flex-c-b">
-                                                                    <div className='flex-center space-x-0.5'>
-                                                                        <span className="flex-center justify-center w-7 h-7 text-primary-gray-70">{detail.id}</span>
+                                                                <td className="flex-c-b gap-5">
+                                                                    <div className='flex-center space-x-0.5 max-440:space-x-2'>
+                                                                        <span className="flex-center justify-center w-7 h-7 text-primary-gray-70 max-640:w-5 max-440:w-2.5">{detail.id}</span>
                                                                         <p className="text-gray-700 text-regular-16">{detail.text}</p>
                                                                     </div>
-                                                                    <span className='text-gray-700 text-regular-16'>{detail.price}₽</span>
+
+                                                                    {/* price  */}
+                                                                    <span className='text-gray-700 text-regular-16 min-w-max'>{detail.price}₽</span>
                                                                 </td>
                                                             </tr>
                                                         )
@@ -54,17 +56,17 @@ const CompletedProjects = () => {
                                         </table>
 
                                         {/* total price  */}
-                                        <div className="flex-e-b mb-auto w-full">
+                                        <div className="flex-e-b mb-auto w-full max-1050:mb-8 max-640:mb-7 max-440:mb-6">
                                             <p className="text-regular-16">Цена за комплект:</p>
                                             <p className="text-regular-24">11 690₽</p>
                                         </div>
 
-                                        <Link to='#' className='red-btn py-3.5 px-6 text-regular-16'>Хочу такой же</Link>
+                                        <Link to='#' className='flex justify-center red-btn py-3.5 px-6 text-regular-16 max-470:w-full'>Хочу такой же</Link>
                                     </div>
 
                                     {/* last child  */}
                                     <div>
-                                        <Swiper className="completed-projects-swiper rounded-2.5xl h-550px"
+                                        <Swiper className="completed-projects-swiper rounded-2.5xl h-550px max-1200:h-513px max-800:h-96 max-640:h-80 max-540:h-72 max-470:h-210px"
                                             effect={'fade'}
                                             slidesPerView={1}
                                             spaceBetween={30}
