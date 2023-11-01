404819507cab49f26dcc5630bb754376767df497
mryaxyobek <pubgn9642@gmail.com>
2023-11-02 01:55:30 +0500

    "offer" component is optimized for all devices

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -9,17 +9,17 @@ const Offer = () => {
         setPhoneNumber(formattedPhoneNumber);
     };
     return (
-        <div className="py-20 container">
-            <div className="flex-c-b rounded-2.5xl py-9 px-60px bg-primary-gray-80">
-                <h3 className="text-white text-medium-28 max-w-687px">Получите бесплатный дизайн-эскиз вашего будущего кабинета</h3>
+        <div className="py-60r container max-470:px-0">
+            <div className="flex-c-b rounded-2.5xl py-9 px-60px bg-primary-gray-80 max-1150:flex-col max-640:px-10 max-470:px-5 max-470:py-10">
+                <h3 className="text-white text-medium-28 max-w-687px max-1150:text-center max-1150:mb-8">Получите бесплатный дизайн-эскиз вашего будущего кабинета</h3>
 
                 {/* form  */}
-                <form action="https://echo.htmlacademy.ru" className="flex items-end space-x-8">
-                    <div className='max-w-sm'>
+                <form action="https://echo.htmlacademy.ru" className="flex items-end space-x-8 min-w-max max-640:flex-col max-640:gap-6 max-640:w-full">
+                    <div className='max-w-sm max-640:w-full max-640:max-w-full'>
                         <label htmlFor='ctatelinput' className="inline-block mb-2 text-regular-14 text-white">Ваш телефон*</label>
                         <input autoComplete='off' id='ctatelinput' maxLength={16} value={phoneNumber} name='phone number' onChange={handlePhoneNumberChange} placeholder='+7 (999) 999-999' required type="tel" className='leading-17.5px!' />
                     </div>
-                    <button className="red-btn py-3.5 px-6 text-regular-16">
+                    <button className="red-btn py-3.5 px-6 text-regular-16 max-640:w-full">
                         Получить эскиз
                     </button>
                 </form>
