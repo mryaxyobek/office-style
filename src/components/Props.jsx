fca697489a3784df4468aa230cc4363d12ce5d86
mryaxyobek <pubgn9642@gmail.com>
2023-11-01 23:23:46 +0500

    "props" section is optimized for all devices

--- src/components/Props.jsx
+++ src/components/Props.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Props = () => {
     return (
-        <section className='mb-24'>
+        <section className='mb-24 max-900:mb-20 max-730:mb-16 max-360:mb-14'>
             <div className="container">
-                <div className="rounded-2.5xl border-primary-gray-10 p-10 border">
-                    <h2 className="mb-8">Реквизиты</h2>
+                <div className="rounded-2.5xl border-primary-gray-10 p-10 border max-900:p-8 max-730:p-6 max-440:p-4">
+                    <h2 className="mb-8 max-730:mb-6 max-440:mb-4">Реквизиты</h2>
                     <p className="text-regular-16 text-primary-gray-70 max-w-md">
                         ООО «МС-Групп» <br /><br /><br /> Юридический адрес: 350051, Краснодарский край г. Краснодар, ул. Дальняя, д. № 27, помещение 5.<br /><br />ИНН: 2308267459<br />КПП: 230801001<br />Р/сч: 40702810026020004092<br />Банк: ФИЛИАЛ «РОСТОВСКИЙ» АО «АЛЬФА-БАНК»<br />БИК: 046015207<br />кор.счет: 30101810500000000207
                     </p>
