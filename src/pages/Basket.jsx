cf58822bad052b6acc95033a877bd206faf61a92
mryaxyobek <pubgn9642@gmail.com>
2023-12-02 11:25:42 +0500

    basket bugs fixed

--- src/pages/Basket.jsx
+++ src/pages/Basket.jsx
@@ -46,6 +46,7 @@ const Basket = () => {
                                         {
                                             product.images.map((img) => {
                                                 return (
+                                                    img.id <= 3 &&
                                                     <SwiperSlide key={img.id} className="flex items-center justify-center relative">
                                                         <img className='absolute brightness-95 img w-full min-h-full object-cover object-center transition-transform-2' width={416} height={310} src={img.img} alt="furniture image" />
                                                         <div className="absolute tabs-wrapper top-5 right-5 transition-opacity-2">
@@ -93,7 +94,17 @@ const Basket = () => {
                                         <li className="flex items-end">
                                             <span>Ustki material</span>
                                             <div className="grow border-t-2 mx-1 mb-0.5 border-primary-gray-70 border-dotted"></div>
-                                            <span>{product.details.material}</span>
+                                            <ul className='flex-center gap-0.5'>
+                                                {
+                                                    product.details.material.map(name => {
+                                                        return (
+                                                            <li key={name.id} className='after:ml-0.5 after:content-["/"] last:after:content-[""] after:text-primary-gray-70'>
+                                                                {name.name}
+                                                            </li>
+                                                        )
+                                                    })
+                                                }
+                                            </ul>
                                         </li>
                                         <li className="flex items-end">
                                             <span>Ustki qalinlik</span>
@@ -105,12 +116,12 @@ const Basket = () => {
                                     {/* price */}
                                     <div className="flex-c-b max-440:flex-col max-440:items-start max-440:gap-6 mb-5">
                                         <div className="flex-end max-340:flex-col max-340:items-start gap-1">
-                                            <span className="inline-block text-regular-16 text-primary-gray-70 mr-1" >narxi:</span>
-                                            <p className="text-regular-20 mr-3">{product.price.currentPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}so'm</p>
+                                            <span className="inline-block text-regular-16 text-primary-gray-70 mr-1" >Narx:</span>
+                                            <p className="text-regular-20 mr-3">{product.parts[0].currentPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}so'm</p>
 
                                             {/* old price aksiya price  */}
                                             {
-                                                product.price.oldPrice && <del className='text-regular-18 text-primary-gray-50'>{product.price.currentPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}so'm</del>
+                                                product.parts[0].oldprice && <del className='text-regular-18 text-primary-gray-50'>{product.parts[0].oldprice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}so'm</del>
                                             }
                                         </div>
 
@@ -119,10 +130,10 @@ const Basket = () => {
 
                                     {/* buttons  */}
                                     <div className="flex flex-co w-full gap-2">
-                                        <Link to={`/catalog/${product.productTitleNoSpacing}`} className="flex-center justify-center red-btn py-2.5 px-5 w-full bg-primary-red-60">Buyurtma berish</Link>
-                                        <button onClick={() => dispatch(deleteCard(product))} className="red-btn py-2.5 px-8 bg-transparent border-2 border-primary-red-60 ">
+                                        <Link to={`/catalog/${product.type.toLowerCase().replace(/\s+/g, '-')}/${product.productTitle.toLowerCase().replace(/\s+/g, '-')}`} className="flex-center justify-center red-btn py-2.5 px-5 w-full bg-primary-red-60">Buyurtma berish</Link>
+                                        <button onClick={() => dispatch(deleteCard(product))} className="red-btn py-2.5 px-8 bg-transparent border-2 border-primary-red-60 hover:bg-white hover:button-hovered">
 
-                                            <svg width={24} height={24} className='text-primary-red-60' clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24"  fill='currentColor' xmlns="http://www.w3.org/2000/svg"><path d="m4.015 5.494h-.253c-.413 0-.747-.335-.747-.747s.334-.747.747-.747h5.253v-1c0-.535.474-1 1-1h4c.526 0 1 .465 1 1v1h5.254c.412 0 .746.335.746.747s-.334.747-.746.747h-.254v15.435c0 .591-.448 1.071-1 1.071-2.873 0-11.127 0-14 0-.552 0-1-.48-1-1.071zm14.5 0h-13v15.006h13zm-4.25 2.506c-.414 0-.75.336-.75.75v8.5c0 .414.336.75.75.75s.75-.336.75-.75v-8.5c0-.414-.336-.75-.75-.75zm-4.5 0c-.414 0-.75.336-.75.75v8.5c0 .414.336.75.75.75s.75-.336.75-.75v-8.5c0-.414-.336-.75-.75-.75zm3.75-4v-.5h-3v.5z" fillRule="nonzero" /></svg>
+                                            <svg width={24} height={24} className='text-primary-red-60 transition-colors-2' clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" fill='currentColor' xmlns="http://www.w3.org/2000/svg"><path d="m4.015 5.494h-.253c-.413 0-.747-.335-.747-.747s.334-.747.747-.747h5.253v-1c0-.535.474-1 1-1h4c.526 0 1 .465 1 1v1h5.254c.412 0 .746.335.746.747s-.334.747-.746.747h-.254v15.435c0 .591-.448 1.071-1 1.071-2.873 0-11.127 0-14 0-.552 0-1-.48-1-1.071zm14.5 0h-13v15.006h13zm-4.25 2.506c-.414 0-.75.336-.75.75v8.5c0 .414.336.75.75.75s.75-.336.75-.75v-8.5c0-.414-.336-.75-.75-.75zm-4.5 0c-.414 0-.75.336-.75.75v8.5c0 .414.336.75.75.75s.75-.336.75-.75v-8.5c0-.414-.336-.75-.75-.75zm3.75-4v-.5h-3v.5z" fillRule="nonzero" /></svg>
                                         </button>
                                     </div>
                                 </li>
